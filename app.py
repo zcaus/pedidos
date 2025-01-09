77046ee8260b31d3fd7118f43cc61a0c525599c0
FOXMIX BRICO <cauamoreira2k@gmail.com>
2025-01-09 08:54:53 -0300

    att

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from io import BytesIO
 st.set_page_config(
     page_title="Controle de Pedidos",
     page_icon="image/mascote_instagram-removebg-preview.png",
-    layout="wide",
     initial_sidebar_state="expanded",
 )
 
@@ -17,7 +16,7 @@ def carregar_pedidos():
     if os.path.exists(FILE_PATH):
         pedidos = pd.read_csv(FILE_PATH, dtype=str)  # Carregar tudo como string inicialmente
     else:
-        pedidos = pd.DataFrame(columns=["Nº Pedido", "Fornecedor", "Qtd.", "Valor (R$)", "Pedido por", "Status" "Recebido por", "Nº NF", "Dt. Receb.", "Hr. Receb.","Tipo Documento", "Valor Incorreto"])
+        pedidos = pd.DataFrame(columns=["Nº Pedido", "Fornecedor", "Qtd.", "Valor (R$)", "Pedido por", "Status" "Recebido por", "Tipo Doc", "Nº", "Dt. Receb.", "Hr. Receb.", "Valor Divergente"])
         pedidos.to_csv(FILE_PATH, index=False)
 
     return pedidos
@@ -39,10 +38,16 @@ def lancar_pedido():
     st.markdown("<h1 style='text-align: center;'>Sistema de Pedidos</h1>", unsafe_allow_html=True)  
 
     with st.form(key='pedido_form'):
-        numero_pedido = st.text_input("Nº Pedido")
-        nome_empresa = st.text_input("Fornecedor")
-        quantidade = st.number_input("Quantidade", min_value=1)
-        valor = st.number_input("Valor", min_value=0.0, format="%.2f")
+        col1, col2 = st.columns(2)
+        with col1:
+            numero_pedido = st.text_input("Nº Pedido")
+        with col2:
+            nome_empresa = st.text_input("Fornecedor")
+        col1, col2 = st.columns(2)
+        with col1:
+            quantidade = st.number_input("Quantidade", min_value=1)
+        with col2:    
+            valor = st.text_input("Valor")
         pedido_por = st.text_input("Pedido por")
         submit_button = st.form_submit_button(label='Lançar Pedido')
         
@@ -60,11 +65,11 @@ def lancar_pedido():
                     "Pedido por": pedido_por,                
                     "Status": "Em trânsito",
                     "Recebido por": "",
-                    "Nº NF": "",
+                    "Nº": "",
                     "Dt. Receb.": "",
                     "Hr. Receb.": "",
-                    "Tipo Documento": "",
-                    "Valor Incorreto": ""
+                    "Tipo Doc": "",
+                    "Valor Divergente": ""
                 }])
                 st.session_state.pedidos = pd.concat([st.session_state.pedidos, novo_pedido], ignore_index=True)
                 salvar_pedidos()
@@ -93,24 +98,29 @@ def confirmar_recebimento():
     st.dataframe(filtered_pedidos, use_container_width=True)
 
     with st.form("receber_pedido_form"):
-        numero_pedido = st.text_input("Nº Pedido")
-        recebido_por = st.text_input("Recebido por")
-
+        col1, col2 = st.columns(2)
+        with col1:
+            numero_pedido = st.text_input("Nº Pedido")
+        with col2:
+            recebido_por = st.text_input("Recebido por")
         col1, col2 = st.columns(2)
         with col1:
             tipo_documento = st.radio("Tipo de Documento", options=["NF", "Recibo"])
         with col2:
-            numero_nf = st.text_input("Número da Nota Fiscal")
-        data_recebimento = st.date_input("Data de Recebimento", datetime.today())
-        horario_atual = datetime.now()
-        horario_ajustado = (horario_atual - timedelta(hours=3)).strftime("%H:%M")
-        hora_recebimento = st.text_input("Hora de Recebimento", horario_ajustado)
+            numero_doc = st.text_input("Nº")
+        col1, col2 = st.columns(2)
+        with col1:
+            data_recebimento = st.date_input("Data de Recebimento", datetime.today())
+        with col2:
+            horario_atual = datetime.now()
+            horario_ajustado = (horario_atual - timedelta(hours=3)).strftime("%H:%M")
+            hora_recebimento = st.text_input("Hora de Recebimento", horario_ajustado)
 
         col1, col2 = st.columns(2)
         with col1:
-            valor_correto = st.radio("Valor correto?", options=["Sim", "Não"])
+            valor_correto = st.radio("Valor divergente?", options=["Sim", "Não"])
         with col2:
-            valor_incorreto = st.text_input("Se não, qual valor?")
+            valor_incorreto = st.text_input("Se sim, qual valor?")
 
         
  
@@ -122,11 +132,11 @@ def confirmar_recebimento():
         elif numero_pedido in st.session_state.pedidos["Nº Pedido"].values:
             index = st.session_state.pedidos[st.session_state.pedidos["Nº Pedido"] == numero_pedido].index[0]
             st.session_state.pedidos.at[index, "Recebido por"] = recebido_por
-            st.session_state.pedidos.at[index, "Nº NF"] = numero_nf
+            st.session_state.pedidos.at[index, "Nº"] = numero_doc
             st.session_state.pedidos.at[index, "Dt. Receb."] = data_recebimento
             st.session_state.pedidos.at[index, "Hr. Receb."] = hora_recebimento
-            st.session_state.pedidos.at[index, "Tipo Documento"] = tipo_documento
-            st.session_state.pedidos.at[index, "Valor Incorreto"] = valor_incorreto
+            st.session_state.pedidos.at[index, "Tipo Doc"] = tipo_documento
+            st.session_state.pedidos.at[index, "Valor Divergente"] = valor_incorreto
             st.session_state.pedidos.at[index, "Status"] = "Recebido"
             salvar_pedidos()
             st.success("Recebimento confirmado!")
